179ead5d5d9e418aca3ff909f3993cc54df983f2
Fiona Snyder <fiona.snyder@outlook.com>
2022-07-10 15:31:16 -0500

    commit tree 02f686e10aafa883bf869d32d75bf56fd631d446

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cards, events)
+{
+    let newArr = [];
+    for (let i = 0; i < cards.length; i++)
+    {
+        newArr.push(`Thank you, ${cards[i]}, for the wonderful ${events} gift!`);
+        console.log(`Thank you, ${cards[i]}, for the wonderful ${events} gift!`);
+    }
+    return newArr;
+}
+writeCards(cards, "surprise");
+
+function countDown()
+{
+    let i = 10;
+    while (i >= 0)
+    {
+        console.log(i);
+        i--;
+    }
+}
+
